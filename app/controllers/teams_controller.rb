d7521e4f5881912d149f29d71d12e958e2e8ec79
rosedive <sakindiuweramarie@gmail.com>
2019-11-13 17:20:04 -0800

    first commit

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,59 @@
+class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_team, only: %i[show edit update destroy]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    @working_team = @team
+    change_keep_team(current_user, @team)
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit; end
+
+  def create
+    @team = Team.new(team_params)
+    @team.owner = current_user
+    if @team.save
+      @team.invite_member(@team.owner)
+      redirect_to @team, notice: 'チーム作成に成功しました！'
+    else
+      flash.now[:error] = '保存に失敗しました、、'
+      render :new
+    end
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to @team, notice: 'チーム更新に成功しました！'
+    else
+      flash.now[:error] = '保存に失敗しました、、'
+      render :edit
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_url, notice: 'チーム削除に成功しました！'
+  end
+
+  def dashboard
+    @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  private
+
+  def set_team
+    @team = Team.friendly.find(params[:id])
+  end
+
+  def team_params
+    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+end
---
+++ diveintopost-develop/app/mailers/assign_mailer.rb
@@ -0,0 +1,9 @@
+class AssignMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def assign_mail(email, password)
+    @email = email
+    @password = password
+    mail to: @email, subject: '登録完了'
+  end
+end
